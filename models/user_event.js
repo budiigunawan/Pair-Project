f5e64bee35ddb5b8fb0843e5d9af7d38c956fe8a
Budi Gunawan <budiindra.g@gmail.com>
2020-03-25 14:21:47 +0700

    user_event extends Model

--- models/user_event.js
+++ models/user_event.js
@@ -3,11 +3,12 @@ module.exports = (sequelize, DataTypes) => {
   const Sequelize = sequelize.Sequelize;
   const Model = Sequelize.Model;
 
+  class User_Event extends Model{}
+
   User_Event.init({
     UserId: DataTypes.INTEGER,
     EventId: DataTypes.INTEGER
-  },
-    {});
+  },{sequelize});
 
   User_Event.associate = function(models) {
     User_Event.belongsTo(models.Event)
