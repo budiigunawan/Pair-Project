7893ecb5f5a0487a68fc0ccc93b1fc6df301f4a8
Budi Gunawan <budiindra.g@gmail.com>
2020-03-26 14:42:42 +0700

    gabungin fix

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
 const express = require('express')
+const route = require('./routes')
 const app = express()
 const port = 3000
-<<<<<<< HEAD
 
-=======
->>>>>>> ba30bdc8ee7faa1befbe7a340e83a53bf890e88e
-const route = require('./routes')
 
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended:true}))
 
 app.use('/',route)
 
-<<<<<<< HEAD
-app.listen(port, ()=>{
-    console.log('This application is running on port ',port)
-})
-=======
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
->>>>>>> ba30bdc8ee7faa1befbe7a340e83a53bf890e88e
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,13 @@
-<<<<<<< HEAD
-const route = require('express').Router()
-const Controller = require('../controllers')
-const userRoute = require('./userRoute')
-
-route.get('/',Controller.home)
-route.use('/users',userRoute)
-=======
 const express = require('express')
 const route = express.Router()
 const events = require('./eventsroute')
-
+const userRoute = require('./userRoute')
 
 route.get('/',(req,res)=>{
     res.render('home.ejs')
 })
 
-
 route.use('/events',events)
-
-
-
->>>>>>> ba30bdc8ee7faa1befbe7a340e83a53bf890e88e
+route.use('/users',userRoute)
 
 module.exports = route
